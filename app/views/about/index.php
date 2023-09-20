a8f4a081a2db951afe6905060baf01704e3a1001
dhikaxzz <afdhikasyahputra@gmail.com>
2023-09-21 06:44:36 +0700

    first commit~

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+    <div class="container">
+        <h1 class="mt-4">About me</h1>
+        <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah <?= $data['pekerjaan']; ?></p>
+        <img src="<?= BASEURL; ?>/img/dhika.jpg" alt="dhikaganteng" width="200" class="rounded-circle shadow">
+    </div>
